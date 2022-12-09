b17ee082ac694b39beb15e183a66f1aa936bdda6
indu <70087467+lazyindu@users.noreply.github.com>
2022-12-09 21:03:55 +0530

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-echo "Cloning Repo...."
-git clone https://github.com/lazyindu/koyebhunter.git /LazyUrlHunterrBOT
-cd /LazyUrlHunterrBOT
-pip3 install -U -r requirements.txt
-echo "Starting Bot.... LazyDeveloperrr"
-python3 main.py
